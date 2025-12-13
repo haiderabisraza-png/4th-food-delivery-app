2f4bd9a7abba13435c871ad78dca024da0e52193
haiderabisraza-png <abisrazahaider@gmail.com>
2025-12-13 15:23:06 +0500

    updated

--- Admin/src/pages/List/List.jsx
+++ Admin/src/pages/List/List.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './List.css'
+import axious from 'axios'
+import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({url}) => {
+  
+  const[list,setList]=useState([]);
+  const fetchList= async()=>{
+    const response= await axious.get(`${url}/api/food/list`);
+    if(response.data.success){
+      setList(response.data.data);
+    }
+    else{
+      toast.error("ERROR");
+    }
+  }
+  const removeFood=async(foodId)=>{
+const response =await axious.post(`${url}/api/food/remove`,{id:foodId});
+await fetchList();
+if(response.data.success){
+  toast.success(response.data.message);
+}
+else{
+  toast.error("Error while removing food item");
+}
+  }
+  useEffect(()=>{
+    fetchList();
+  },[]);
+
   return (
-    <div>
-      
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index)=>{
+return(
+  <div className="list-table-format" key={index}>
+          <img src={`${url}/images/`+item.image} alt="" />
+          <p>{item.name}</p>
+          <p>{item.category}</p>
+          <p>${item.price}</p>
+          <p className='cursor' onClick={()=>removeFood(item._id)}>X</p>
+  </div>
+)
+        })}
+      </div>
     </div>
   )
 }
---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,15 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js"
+import Stripe from "stripe"
+
+
+const stripe=new Stripe(process.env.STRIPE_SECRET_KEY);
+
+
+//place order from frontend
+const placeOrder=async(req,res)=>{
+
+
+}
+
+export {placeOrder};
